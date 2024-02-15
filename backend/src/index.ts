128b15f252059cd353e0e918e3189d187fccb818
Erik Malm <malm.erik@gmail.com>
2024-02-15 17:37:12 +0100

    chore: Setup of backend and basic types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
+import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+dotenv.config();
+//console.log("STARTING UP! ",process.env);
 
 app.get("/", (req: Request, res: Response) => {
   console.log("req", req);
