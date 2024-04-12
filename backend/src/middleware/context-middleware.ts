267fde6cf62a9fe666bd0298873fec5f3ad3cf30
henrik-westoo <henrik.westoo@etimo.se>
2024-04-12 13:14:04 +0200

    format

--- backend/src/middleware/context-middleware.ts
+++ backend/src/middleware/context-middleware.ts
@@ -5,29 +5,23 @@ import { fromBase64 } from "../utils/base64-helper";
 export const contextMiddleware = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const sessionCookie = req.headers["cookie"];
-
     if (!sessionCookie) {
       return res.status(401).json({ error: "Unauthorized" });
     }
 
-    const regexForSessionCookie = /session=([^;].+)/;
-    const sessionCookieMatch = sessionCookie.match(regexForSessionCookie);
-
+    const sessionCookieMatch = sessionCookie.match(/session=([^;].+)/);
     if (!sessionCookieMatch) {
       return res.status(401).json({ error: "Unauthorized" });
     }
 
-    const sessionCookieValue = sessionCookieMatch[1];
-    const userId = fromBase64(sessionCookieValue);
+    const userId = fromBase64(sessionCookieMatch[1]);
     const user = await UserRepository.getById(userId);
-
     if (!user) {
       return res.status(401).json({ error: "Unauthorized" });
     }
 
     // http://expressjs.com/en/api.html#res.locals
     res.locals.user = user;
-
     next();
   };
 };
