86b5c72f5270ba6b1b9037043167904ee34e37ad
henrik-westoo <henrik.westoo@etimo.se>
2024-06-16 18:01:06 +0200

    persistent db

--- backend/src/configuration/mongo.ts
+++ backend/src/configuration/mongo.ts
@@ -1,12 +1,8 @@
 import mongoose from "mongoose";
 //const uri = process.env.MONGO_DB_CONNECTION_STRING ?? "";
 
-export const connectDb = async (connectionString:string) => {
-console.log("Connectiong to", connectionString)
-const connection = await mongoose.connect(
-    connectionString
-   );
-return connection;
-}
-
-
+export const connectDb = async (connectionString: string) => {
+  console.log("Connecting to", connectionString);
+  const connection = await mongoose.connect(connectionString);
+  return connection;
+};
